dfde074d40a67470096e5b141a66a5dece100614
byd <byandong@gmail.com>
2015-10-21 16:09:42 +0800

    红外检测测试部分，上传到redis状态服务器

--- steer/infrared/human_detect.py
+++ steer/infrared/human_detect.py
@@ -1,16 +1,42 @@
-__author__ = 'byd'
+#!/usr/bin/env python
+# encoding: utf-8
 
 import time
+import ConfigParser
 
-import RPi.GPIO as GPIO
+import redis
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(12, GPIO.IN)
-while True:
-    in_value = GPIO.input(12)
-    if in_value == False:
-        GPIO.output(11, False)
-        time.sleep(1)
-        GPIO.output(11, True)
-        while in_value == False:
-            in_value = GPIO.input(12)
+import RPi.GPIO
+
+'''检测是否有人存在，将状态数据发送到服务器'''
+
+cf = ConfigParser.ConfigParser(allow_no_value=True)
+try:
+    cf.read("./conf.ini")
+    port = cf.getint("rspi", 'port')
+    interval = cf.getfloat('service', 'send_interval')
+
+    redis_host = cf.get('service', 'host')
+    redis_port = cf.getint('service', 'port')
+    redis_db = cf.getint('service', 'db')
+    desc = cf.get('service', 'description')
+    id = cf.get('service', 'id')
+    rds = redis.StrictRedis(host=redis_host, port=redis_port, db=redis_db)
+    res = rds.hset('dev_info', id, desc)
+    print 'new device added' if res == 1 else 'device already registered'
+
+    RPi.GPIO.setmode(RPi.GPIO.BCM)
+    RPi.GPIO.setup(port, RPi.GPIO.IN)
+    RPi.GPIO.setwarnings(False)
+    print 'ready sending detect result to', redis_host, 'at interval of', interval
+
+    while True:
+        try:
+            rds.hset('dev_value', id, RPi.GPIO.input(port))
+            time.sleep(interval)
+        except Exception, e:
+            pass
+    RPi.GPIO.cleanup()
+except Exception, e:
+    print e
+    exit(1)
